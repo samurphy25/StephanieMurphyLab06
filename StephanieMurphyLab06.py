ce1e9e2be99ebfee8bc493bf978dbd590e18bd57
samurphy25 <murphy.s@ufl.edu>
2023-03-27 18:13:29 -0400

    lab06 addition to repo

---
+++ StephanieMurphyLab06.py
@@ -0,0 +1,32 @@
+def menu_function():
+    print("Menu option:")
+    print("1. Encode password")
+    print("2. Decode password")
+    print("3. Exit")
+    print(end="\n")
+
+
+def encoder_function(digits):
+    result = ""
+    for num in digits:
+        new_numbers = str((int(num) + 3))
+        result += new_numbers
+    return result
+
+
+if __name__ == "__main__":
+    while True:
+        menu_function()
+        option = int(input("Choose to encode or decode your password:"))
+        print(end="\n")
+
+        if option == 1:
+            password = (input("Enter your 8-digit password here:"))
+            print(encoder_function(password))
+            print(end="\n")
+
+        elif option == 2:
+            pass
+
+        elif option == 3:
+            break
