e8b14cd16bdd2c8dcadfe1efdbd34fd7156447d4
RamziDiehs <diehsramzi@ufl.edu>
2023-03-27 20:13:29 -0400

    added decoding, function w menu, fixed error in encode

--- StephanieMurphyLab06.py
+++ StephanieMurphyLab06.py
@@ -1,4 +1,5 @@
 #  stephanie murphy
+#Ramzi diehs
 def menu_function():
     print("Menu option:")
     print("1. Encode password")
@@ -10,24 +11,38 @@ def menu_function():
 def encoder_function(digits):
     result = ""
     for num in digits:
-        new_numbers = str((int(num) + 3))
-        result += new_numbers
+        new_numbers = (int(num) + 3)
+        if new_numbers > 9:
+            new_numbers = new_numbers % 10
+        result += str(new_numbers)
     return result
+
+def decodePass(encryptedCode): #ramzi did this
+    res = ""
+    for i in encryptedCode:
+        temp = int(i) - 3
+        if temp < 1:
+            temp = abs(temp % 10)
+        res = res + str(temp)
+    return res
+
 
 
 if __name__ == "__main__":
     while True:
         menu_function()
         option = int(input("Choose to encode or decode your password:"))
-        print(end="\n")
+
 
         if option == 1:
             password = (input("Enter your 8-digit password here:"))
-            print(encoder_function(password))
-            print(end="\n")
+            encoded_pass = encoder_function(password)
+            print("Your password has been encoded and stored!")
+            print()
+
 
         elif option == 2:
-            pass
+            print(f"The encoded password is {encoded_pass}, and the original password is {decodePass(encoded_pass)}.")
 
         elif option == 3:
             break
